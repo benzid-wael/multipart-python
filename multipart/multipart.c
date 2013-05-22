3167e0064995677d8c8f9277d88ce871ac6696a6
Eric Urban <hydrogen18@gmail.com>
2013-05-22 00:38:04 -0400

    Nowhere near complete, but successful extracting headers

--- multipart/multipart.c
+++ multipart/multipart.c
@@ -154,7 +154,6 @@ static bool queuePush(multipart_Parser * const self)
   
 static int multipart_Parser_on_header_field(void * actor, const char * data, size_t length)
 {
-	
 	printf("header field %zu bytes:%.*s\n",length,(int)length,data);
 	multipart_Parser * const self = actor;
 	
@@ -228,6 +227,51 @@ static int multipart_Parser_on_header_value_end(void * actor)
 	printf("%.*s=%.*s\n",(int)self->headerFieldLength,self->headerFieldInProgress,
 	(int)self->headerValueLength,self->headerValueInProgress);
 	
+	//Null terminate both buffers
+	self->headerFieldInProgress[self->headerFieldLength] = '\0';
+	self->headerValueInProgress[self->headerValueLength] = '\0';
+	
+	PyObject * const field = PyString_FromString(self->headerFieldInProgress);
+	PyObject * const value = PyString_FromString(self->headerValueInProgress);
+	
+	if(not value or not field)
+	{
+		Py_XDECREF(field);
+		Py_XDECREF(value);
+		PyErr_NoMemory();
+		return 1;
+	}
+	
+	PyObject * const tuple = PyTuple_Pack(2,field,value);
+	Py_DECREF(field);
+	Py_DECREF(value);
+	
+	if(not tuple)
+	{
+		PyErr_NoMemory();
+		return 1;
+	}
+	
+	PyObject * const push = PyObject_GetAttrString(self->iteratorQueue[self->currentIteratorPair*2],"push");
+	
+	if(not push)
+	{
+		PyErr_SetString(PyExc_NameError,"Cannot find Generator.push");
+		Py_DECREF(tuple);
+		return 1;
+	}
+	
+	PyObject * const result = PyObject_Call(push,tuple,NULL);
+	Py_DECREF(tuple);
+	Py_DECREF(push);
+	
+	if(not result)
+	{
+		return 1;
+	}
+	
+	Py_DECREF(result);
+	
 	self->headerValueLength = 0;
 	self->headerFieldLength = 0;
 	return 0;
@@ -235,8 +279,28 @@ static int multipart_Parser_on_header_value_end(void * actor)
 
 static int multipart_Parser_on_headers_complete(void * actor)
 {
+	printf("on_headers_complete\n");
 	multipart_Parser * const self = actor;
 	self->headersComplete = true;
+	
+	PyObject * const done = PyObject_GetAttrString(self->iteratorQueue[self->currentIteratorPair*2],"done");
+	
+	if(not done)
+	{
+		PyErr_SetString(PyExc_NameError,"Cannot find Generator.done");
+		return 1;
+	}
+	
+	PyObject * const emptyTuple = PyTuple_Pack(0);
+	
+	PyObject * const result = PyObject_Call(done,emptyTuple,NULL);
+	Py_DECREF(result);
+	Py_DECREF(emptyTuple);
+	
+	if(not result)
+	{
+		return 1;
+	}
 	
 	return 0;
 }
@@ -331,7 +395,6 @@ static PyObject* Parser_iter(PyObject * self)
 
 static PyObject* Parser_read(multipart_Parser * const self, PyObject * unused0, PyObject * unused1)
 {
-
 	PyObject * const i = PyIter_Next(self->readIterator);
 	
 	if(i == NULL)
@@ -403,22 +466,33 @@ static PyObject* Parser_iternext(multipart_Parser * const self)
 	
 	//Check to see if the iterator pair being returned is getting ahead
 	//of the iterator pair being populated
+	
+	PyObject * const emptyTuple = PyTuple_New(0);
+	
 	while(self->outgoingIteratorPair > self->currentIteratorPair)
 	{
-		PyObject * const callresult = PyObject_Call(read,NULL,NULL);
+		printf("Parser_iternext\n");
+		if(self->dataComplete)
+		{
+			Py_DECREF(emptyTuple);
+			Py_DECREF(read);
+			return NULL;
+		}
+
+		PyObject * const callresult = PyObject_Call(read,emptyTuple,NULL);
 		
 		if(not callresult)
 		{
+			Py_DECREF(emptyTuple);
+			Py_DECREF(read);
 			return NULL;
 		}
 		
 		Py_DECREF(callresult);
-		
-		if(self->dataComplete)
-		{
-			return NULL;
-		}
 	}
+	
+	Py_DECREF(read);
+	Py_DECREF(emptyTuple);
 	
 	PyObject * retval = PyTuple_Pack(2,self->iteratorQueue[self->outgoingIteratorPair*2],self->iteratorQueue[self->outgoingIteratorPair*2+1]);
 	
@@ -437,9 +511,120 @@ typedef struct
 {
 	PyObject_HEAD
 	PyObject * callback;
+	
+	PyObject ** queue;
+	size_t queueSize;
+	size_t queueLength;
+	size_t queueRead;
+	
+	bool done;
 }multipart_Generator;
 
-static PyMethodDef Generator_methods[] = { {NULL} };
+static PyObject * Generator_iter(PyObject * const self)
+{
+	Py_INCREF(self);
+	return self;
+}
+
+static PyObject * Generator_iternext(multipart_Generator * const self)
+{
+	
+	PyObject * const emptyTuple = PyTuple_New(0);
+	
+	while(self->queueRead == self->queueLength)
+	{
+		if(self->done)
+		{
+			printf("Generator_iternext-done\n");
+			return NULL;
+		}
+		
+		PyObject * const result = PyObject_Call(self->callback,emptyTuple,NULL);
+		
+		if(not result)
+		{
+			Py_DECREF(emptyTuple);
+			return NULL;
+		}
+		
+		Py_DECREF(result);
+		//printf("Generator_iternext\n");
+	}
+	Py_DECREF(emptyTuple);
+	
+	PyObject * const retval = self->queue[self->queueRead];
+	self->queue[self->queueRead] = NULL;
+	self->queueRead += 1;
+	
+	return retval;
+
+}
+
+static void Generator_dealloc(multipart_Generator * self)
+{
+	for(size_t i = self->queueRead; i < self->queueLength; ++i)
+	{
+		Py_DECREF(self->queue[i]);
+	}
+	
+	PyMem_Free(self->queue);
+	Py_DECREF(self->callback);
+}
+static PyObject * Generator_done(multipart_Generator * self, PyObject *args, PyObject *kwds)
+{
+	printf("Generator_done\n");
+	self->done = true;
+	Py_RETURN_NONE;
+}
+
+static PyObject * Generator_push(multipart_Generator * self, PyObject *args, PyObject *kwds)
+{
+	PyObject * item;
+	
+	if(PyTuple_Size(args)==1)
+	{
+		if( not PyArg_ParseTuple(args,"O",&item) )
+		{
+			return NULL;
+		}
+	}
+	else
+	{
+		item = args;
+	}
+	
+	Py_INCREF(item);
+	
+	const size_t newLength = self->queueLength +1;
+	
+	if(newLength > self->queueSize)
+	{
+		if(self->queueRead != 0)
+		{
+			memmove(self->queue,self->queue + self->queueRead,self->queueLength - self->queueRead  * sizeof(PyObject*));
+			self->queueLength -= self->queueRead;
+			self->queueRead = 0;
+		}
+		else
+		{
+			self->queueSize *= 2;
+			const int NEW_SIZE = sizeof(PyObject*)*self->queueSize;
+			self->queue = PyMem_Realloc(self->queue,NEW_SIZE);
+		}
+	}
+	
+	self->queue[self->queueLength] = item;
+	self->queueLength += 1;
+	
+	Py_RETURN_NONE;
+}
+
+static PyMethodDef Generator_methods[] = 
+{ 
+	{"push",(PyCFunction)Generator_push,METH_KEYWORDS,"push an object into the generator"} ,
+	{"done",(PyCFunction)Generator_done,METH_KEYWORDS,"signal the iterator to end the data stream"},
+	{NULL} 
+};
 static PyMemberDef Generator_members[] = { {NULL} };
 
 static int Generator_init(multipart_Generator * self, PyObject *args, PyObject *kwds)
@@ -452,9 +637,27 @@ static int Generator_init(multipart_Generator * self, PyObject *args, PyObject *
 	}
 	
 	self->callback = callback;
+	Py_INCREF(self->callback);
+	
+	static const int STARTING_SIZE = 4;
+	const int SIZE_BYTES = sizeof(PyObject*)*STARTING_SIZE;
+	self->queue = PyMem_Malloc(SIZE_BYTES);
+	if(not self->queue)
+	{
+		PyErr_NoMemory();
+		return -1;
+	}
+	bzero(self->queue,SIZE_BYTES);
+	
+	self->queueSize = STARTING_SIZE;
+	self->queueLength = 0;
+	self->queueRead = 0;
+	self->done = false;
 	
 	return 0;
 }
+
+
 
 static PyTypeObject multipart_GeneratorType = {
 	PyObject_HEAD_INIT(NULL)
@@ -462,7 +665,7 @@ static PyTypeObject multipart_GeneratorType = {
     "multipart.CallbackGenerator",             /*tp_name*/
     sizeof(multipart_Generator), /*tp_basicsize*/
     0,                         /*tp_itemsize*/
-    0                          ,/*tp_dealloc*/
+    (destructor)Generator_dealloc          ,/*tp_dealloc*/
     0,                         /*tp_print*/
     0,                         /*tp_getattr*/
     0,                         /*tp_setattr*/
@@ -483,8 +686,8 @@ static PyTypeObject multipart_GeneratorType = {
     0,		               /* tp_clear */
     0,		               /* tp_richcompare */
     0,		               /* tp_weaklistoffset */
-    0,		               /* tp_iter */
-    0,		               /* tp_iternext */
+    Generator_iter,		               /* tp_iter */
+    Generator_iternext,		               /* tp_iternext */
     Generator_methods,             /* tp_methods */
     Generator_members,             /* tp_members */
     0,                         /* tp_getset */
--- tests/multipart_test.py
+++ tests/multipart_test.py
@@ -9,17 +9,21 @@ class TestMultipart(unittest.TestCase):
 		multipart.Parser('x',[])
 		
 	def test_0(self):
-		for part in  multipart.Parser('------------------------------75766a6a01a6',open('tests/test0.txt')):
-			print part
+		for headers,body in  multipart.Parser('------------------------------75766a6a01a6',open('tests/test0.txt')):
+			print 'begin headers'
+			for header in headers:
+				print 'HEADER=' + str(header)
+			print 'end headers'
 		
 	def test_1(self):
+		return
 		def wrapper(i):
 				for j in i:
 					for k in j:
 						yield k
-		for part in multipart.Parser('------------------------------75766a6a01a6',wrapper(open('tests/test0.txt'))):
-			print part
-					
+		for headers,body in multipart.Parser('------------------------------75766a6a01a6',wrapper(open('tests/test0.txt'))):
+			for header in headers:
+				print header					
 		
 if __name__ == '__main__':
     unittest.main()
