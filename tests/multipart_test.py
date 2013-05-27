59d894f97457b8afe74d6592ca13f299a5e1827d
Eric Urban <hydrogen18@gmail.com>
2013-05-27 18:25:42 -0400

    Created unit test to cause parser to fail.

--- tests/multipart_test.py
+++ tests/multipart_test.py
@@ -1,6 +1,7 @@
 import multipart
 import unittest
 import hashlib
+import random
 class TestMultipart(unittest.TestCase):
 	def setUp(self):
 		pass
@@ -55,6 +56,43 @@ class TestMultipart(unittest.TestCase):
 				#print 'LEN=' + str(len(d))						
 				chksum.update(d)
 				
+			self.assertTrue(chksum.hexdigest() == digest)
+			
+	def test_7(self):
+		random.seed(1)
+		
+		def wrapper(i):
+			allOfIt = i.read();
+			
+			while len(allOfIt) != 0:
+				x = min(len(allOfIt),random.randint(4,64))
+				yield allOfIt[:x]
+				allOfIt = allOfIt[x:]
+				
+		
+		digests = \
+		['e3fb78474a477c528d92d01d4fc85a04',# random0
+		'0a5e6db148276bc7e3d5854179ecbf6e',# random1
+		'0a9fdb5ca02b919cb647f5c726d519b6',# random2
+		'686c70dee5e998275508d88c0f3390c4',# random3
+		'9b5ebc254dc324aae1f7366b1d01cb8f',# random4
+		'74dbb0e2ffdab211004aff8a98c58906',# random5
+		'62250b57c1f145f2baf212df3dab4945',# random6
+		'de542ac70dd1f67d9b2b8fb25004d23d',# random7
+		'b20b7cecab7ec5b610a3748431a78d34'] # random8
+		for part,digest in zip(multipart.Parser('------------------------------8f9710048d91',wrapper(open('tests/test1.txt'))),digests):
+			headers, data = part
+			for header in headers:
+				print 'HEADER=' + str(header)
+			
+			chksum = hashlib.md5()
+			length = 0
+			for d in data:
+				#print 'LEN=' + str(len(d))						
+				chksum.update(d)
+				length += len(d)
+				
+			print 'length: %i' %length
 			self.assertTrue(chksum.hexdigest() == digest)
 			
 	def test_4(self):
