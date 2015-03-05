282ffe0ee238cd347bca30d4e40a2481c9452a0d
Wael BEN ZID <benzid.wael@hotmail.fr>
2015-03-05 14:27:20 +0100

    White spaces removed

--- tests/multipart_test.py
+++ tests/multipart_test.py
@@ -1,28 +1,32 @@
+# -*- coding: utf-8 -*-
+
 import multipart
 import unittest
 import hashlib
 import random
+
+
 class TestMultipart(unittest.TestCase):
 	def setUp(self):
 		pass
-		
+
 	def test_badConstruction(self):
 		self.assertRaises(TypeError,multipart.Parser)
 		self.assertRaises(TypeError,multipart.Parser,'foo')
 		self.assertRaises(TypeError,multipart.Generator)
-		
+
 	def test_create(self):
 		multipart.Parser('x',[])
-		
+
 	def test_0(self):
 		for headers,data in  multipart.Parser('------------------------------75766a6a01a6',open('tests/test0.txt')):
-			
+
 			for header in headers:
 				print 'HEADER=' + str(header)
-			
+
 			for d in data:
 				pass#print 'LEN=' + str(len(d))
-		
+
 	def test_1(self):
 		def wrapper(i):
 				for j in i:
@@ -31,10 +35,10 @@ class TestMultipart(unittest.TestCase):
 		for headers,data in multipart.Parser('------------------------------75766a6a01a6',wrapper(open('tests/test0.txt'))):
 			for header in headers:
 				print 'HEADER=' + str(header)
-			
+
 			for d in data:
-				pass#print 'LEN=' + str(len(d))			
-				
+				pass#print 'LEN=' + str(len(d))
+
 	def test_3(self):
 		digests = \
 		['e3fb78474a477c528d92d01d4fc85a04',# random0
@@ -50,26 +54,25 @@ class TestMultipart(unittest.TestCase):
 			headers, data = part
 			for header in headers:
 				print 'HEADER=' + str(header)
-			
+
 			chksum = hashlib.md5()
 			for d in data:
-				#print 'LEN=' + str(len(d))						
+				#print 'LEN=' + str(len(d))
 				chksum.update(d)
-				
-			self.assertTrue(chksum.hexdigest() == digest)
-			
+
+                self.assertTrue(chksum.hexdigest() == digest)
+
 	def test_7(self):
 		random.seed(1)
-		
+
 		def wrapper(i):
 			allOfIt = i.read();
-			
-			while len(allOfIt) != 0:
+                        while len(allOfIt) != 0:
 				x = min(len(allOfIt),random.randint(4,64))
 				yield allOfIt[:x]
 				allOfIt = allOfIt[x:]
-				
-		
+
+
 		digests = \
 		['e3fb78474a477c528d92d01d4fc85a04',# random0
 		'0a5e6db148276bc7e3d5854179ecbf6e',# random1
@@ -84,17 +87,17 @@ class TestMultipart(unittest.TestCase):
 			headers, data = part
 			for header in headers:
 				print 'HEADER=' + str(header)
-			
+
 			chksum = hashlib.md5()
 			length = 0
 			for d in data:
-				#print 'LEN=' + str(len(d))						
+				#print 'LEN=' + str(len(d))
 				chksum.update(d)
 				length += len(d)
-				
+
 			print 'length: %i' %length
 			self.assertTrue(chksum.hexdigest() == digest)
-			
+
 	def test_4(self):
 		digests = \
 		['e3fb78474a477c528d92d01d4fc85a04',# random0
@@ -111,9 +114,9 @@ class TestMultipart(unittest.TestCase):
 			chksum = hashlib.md5()
 			for d in data:
 				chksum.update(d)
-				
+
 			self.assertTrue(chksum.hexdigest() == digest)
-			
+
 	def test_5(self):
 		digests = \
 		['e3fb78474a477c528d92d01d4fc85a04',# random0
@@ -130,14 +133,14 @@ class TestMultipart(unittest.TestCase):
 			if skip:
 				skip = not Skip
 				continue
-				
+
 			_, data = part
 			chksum = hashlib.md5()
 			for d in data:
 				chksum.update(d)
-				
+
 			self.assertTrue(chksum.hexdigest() == digest)
-			
+
 	def test_6(self):
 		digests = \
 		['e3fb78474a477c528d92d01d4fc85a04',# random0
@@ -148,15 +151,15 @@ class TestMultipart(unittest.TestCase):
 			if skip:
 				skip = not Skip
 				continue
-				
+
 			_, data = part
 			chksum = hashlib.md5()
 			for d in data:
 				chksum.update(d)
-				
-			self.assertTrue(chksum.hexdigest() == digest)			
 
-		
-		
+			self.assertTrue(chksum.hexdigest() == digest)
+
+
+
 if __name__ == '__main__':
     unittest.main()
